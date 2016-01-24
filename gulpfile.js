322cee051bcde54e9ae92dff5094404f698cf4fc
Anis BOUHACHEM <anis.bouhachem@tritux.com>
2016-01-24 15:49:07 +0100

    Add gulp server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
+var connect = require('gulp-connect');
+
+var config = {
+  env : process.env.NODE_ENV || "development"
+}
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: config.env.NODE_ENV === "development"
+  });
+});
+
 gulp.task('default', function() {
   gutil.log("Default gulp task");
 });
